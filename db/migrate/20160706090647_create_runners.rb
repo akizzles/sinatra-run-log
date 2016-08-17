f4e112af0f9cdf5e135d6816130d36e38d2269a0
akizzles <aykim002@gmail.com>
2016-08-17 09:21:51 -0400

    removed email column and from form view of new runner

--- db/migrate/20160706090647_create_runners.rb
+++ db/migrate/20160706090647_create_runners.rb
@@ -2,7 +2,6 @@ class CreateRunners < ActiveRecord::Migration
   def change
     create_table :runners do |t|
       t.string :username
-      t.string :email
       t.string :password_digest
     end
   end
